0208b06791a98f7ff8c540f5983acf06414efe04
Hacker-C <1507559148@qq.com>
2021-12-13 21:22:12 +0800

    feature: 我的账号界面数据动态获取显示

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -9,15 +9,25 @@
         :column="3"
         border
       >
-        <el-descriptions-item label="用户名">zhangsan</el-descriptions-item>
-        <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-        <el-descriptions-item label="住址">苏州市</el-descriptions-item>
-        <el-descriptions-item label="性别">男</el-descriptions-item>
-        <el-descriptions-item label="年龄">18</el-descriptions-item>
-        <el-descriptions-item label="姓名">张三</el-descriptions-item>
+        <el-descriptions-item label="用户名">{{
+          user.username
+        }}</el-descriptions-item>
+        <el-descriptions-item label="手机号">{{
+          user.telephone
+        }}</el-descriptions-item>
+        <el-descriptions-item label="住址">{{
+          user.address
+        }}</el-descriptions-item>
+        <el-descriptions-item label="性别">{{ user.sex }}</el-descriptions-item>
+        <el-descriptions-item label="年龄">{{ user.age }}</el-descriptions-item>
+        <el-descriptions-item label="姓名">{{
+          user.realName
+        }}</el-descriptions-item>
         <el-descriptions-item label="头像">
           <div style="position: relative">
-            <el-avatar size="medium"></el-avatar>
+            <el-avatar size="medium"
+              ><img :src="user.avatar" alt=""
+            /></el-avatar>
             <el-button
               size="mini"
               style="position: absolute; top: 5px; left: 60px"
@@ -29,22 +39,22 @@
       <el-descriptions
         title="账号安全"
         direction="vertical"
-        style="margin-top: 10px; margin-bottom: 30px"
+        style="margin-top: 10px; margin-bottom: 50px"
         :column="3"
         border
       >
-        <el-descriptions-item label="账号注册时间" :span="1"
-          >2021-12-10</el-descriptions-item
-        >
+        <el-descriptions-item label="账号注册时间" :span="1">{{
+          user.createTime
+        }}</el-descriptions-item>
         <el-descriptions-item label="在线状态">
           <el-tag size="medium">在线</el-tag>
         </el-descriptions-item>
         <el-descriptions-item label="账号状态">
-          <el-tag size="medium" type="success">正常</el-tag>
+          <el-tag size="medium" type="success">{{ statusText }}</el-tag>
         </el-descriptions-item>
         <el-descriptions-item label="账户余额">
           <div style="position: relative">
-            ￥1230
+            ￥{{ user.money }}
 
             <el-button size="mini" style="position: absolute; right: 0"
               >充值金额</el-button
@@ -55,7 +65,7 @@
           <el-button size="mini">修改密码</el-button>
         </el-descriptions-item>
         <el-descriptions-item label="身份角色">
-          <el-tag>会员客户</el-tag>
+          <el-tag size="medium">会员客户</el-tag>
         </el-descriptions-item>
       </el-descriptions>
     </div>
@@ -64,13 +74,44 @@
 </template>
 
 <script>
+import request from '../utils/request'
 export default {
   data() {
     return {
-      isLogin: sessionStorage.getItem('userId')
+      isLogin: sessionStorage.getItem('userId'),
+      user: {}
     }
   },
-  created() {}
+  created() {
+    this.load()
+  },
+  computed: {
+    statusText() {
+      return this.user.status === 1 ? '正常' : '异常'
+    }
+  },
+  methods: {
+    load() {
+      let userId = sessionStorage.getItem('userId')
+      if (userId) {
+        request
+          .get('/user/' + userId)
+          .then((res) => {
+            if (res.code == '0') {
+              this.$message({
+                message: res.msg,
+                type: 'success',
+                duration: 1000
+              })
+              this.user = Object.assign({}, this.user, res.data)
+            }
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
+    }
+  }
 }
 </script>
 
