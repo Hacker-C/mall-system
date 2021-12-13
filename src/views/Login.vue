8528c5b3cfd5f0ccdef04fac36abcee6f19f7640
Hacker-C <1507559148@qq.com>
2021-12-13 20:43:48 +0800

    feature: 网页logo更换

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,12 +3,14 @@
     <div class="container">
       <img class="left" src="../assets/upload/left.svg" alt="" />
       <img class="right" src="../assets/upload/right.svg" alt="" />
-      <div class="logo" @click="toHome" title="去往首页">
-        <div>
-          <img src="../assets/upload/logo.svg" alt="" style="margin-top: 2px" />
-        </div>
-        <div style="margin-left: 3px">在线商城</div>
-      </div>
+      <button
+        class="button-29 logo"
+        role="button"
+        @click="toHome"
+        title="去往首页"
+      >
+        <img src="../assets/upload/logo.svg" alt="" />在线商城
+      </button>
       <div class="login">
         <el-form
           :rules="rules"
@@ -123,7 +125,7 @@ export default {
 .logo {
   position: relative;
   left: 10px;
-  top: 5px;
+  top: 10px;
   height: 40px;
   width: 120px;
   border-radius: 40px;
@@ -134,10 +136,6 @@ export default {
   align-items: center;
   color: #fff;
   cursor: pointer;
-}
-
-.logo:hover div {
-  text-decoration: underline;
 }
 
 .wel {
@@ -181,5 +179,58 @@ export default {
 .right {
   position: absolute;
   right: 10px;
+}
+
+/* CSS */
+.button-29 {
+  align-items: center;
+  appearance: none;
+  background-image: radial-gradient(
+    100% 100% at 100% 0,
+    #5adaff 0,
+    #00c6ff 100%
+  );
+  border: 0;
+  border-radius: 20px;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, rgba(58, 65, 111, 0.5) 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: 'JetBrains Mono', monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow 0.15s, transform 0.15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow, transform;
+  font-size: 18px;
+}
+
+.button-29:focus {
+  box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+}
+
+.button-29:hover {
+  box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #409eff 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+.button-29:active {
+  box-shadow: #3c4fe0 0 3px 7px inset;
+  transform: translateY(2px);
 }
 </style>
